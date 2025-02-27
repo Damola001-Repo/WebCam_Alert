0794d1f7131e0b92a81a9800196b502dcf4b52aa
Surface <surface@gmail.com>
2025-02-27 08:04:36 -0800

    Wrote send_email function

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import cv2
 import time
 from send_email import send_email
+from glob import glob
 
 video = cv2.VideoCapture(1)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 0
 while True:
     status = 0
     check, frame = video.read()
@@ -18,19 +20,28 @@ while True:
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
 
-    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    cv2.imshow('hw', dil_frame)
+
     for contour in contours:
-        if cv2.contourArea(contour) < 20000:
+        if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
+
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            file_names = glob('images/*.png')
+            index = int(len(file_names)/2)
+            image = file_names[index]
+            print(image)
 
     status_list.append(status)
     status_list = status_list[-2:]
